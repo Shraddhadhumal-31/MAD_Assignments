d8943da9a7673424b68d3aa5f8928b84f571f216
Shraddhadhumal-31 <shraddhadhumal31@gmail.com>
2026-04-08 23:20:21 +0530

    Create MainActivity.java

---
+++ Assignment No.10/internalstorage/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.internalstorage;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btnSave;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();
+
+                try {
+                    FileOutputStream fos = openFileOutput("myfile.txt", MODE_PRIVATE);
+                    fos.write(data.getBytes());
+                    fos.close();
+
+                    Toast.makeText(MainActivity.this, "Data Saved Successfully", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Error Saving Data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
